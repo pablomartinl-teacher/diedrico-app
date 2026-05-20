ded8dfe3ef1a3ad2e1d1891af2ac648d05604aa0
pablomartinl-teacher <pablo.martinl@colegioinfantes.com>
2026-05-20 19:07:51 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/diedrico-app/', // <-- AÑADE ESTA LÍNEA (ej: '/diedrico-app/')
   plugins: [react()],
 })
